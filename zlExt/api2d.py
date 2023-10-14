be3c6b6cd985a37c528a7a7c9ef4957fe0414a9a
ByteDance <zhangyunnian@bytedance.com>
2023-10-15 01:29:06 +0800

    modify

--- zlExt/api2d.py
+++ zlExt/api2d.py
@@ -1,20 +1,30 @@
 import requests
-from zlExt.config import zlConfig
+# from zlExt.config import zlConfig
+
+# def getAnswer(question: str):
+#     try:
+#         url = "https://openai.api2d.net/v1/chat/completions"
+#         headers = {
+#             'Content-Type': 'application/json',
+#             'Authorization': f'Bearer {zlConfig.fkey}' # <-- 把 fkxxxxx 替换成你自己的 Forward Key，注意前面的 Bearer 要保留，并且和 Key 中间有一个空格。
+#         }
+#         data = {
+#             "model": "gpt-3.5-turbo",
+#             "messages": [{"role": "user", "content": question}]
+#         }
+
+#         response = requests.post(url, headers=headers, json=data)
+#         data = response.json()
+#         return data['choices'][0]['message']['content']
+#     except Exception as e:
+#         return f'[错误]: 获取答复失败！'
 
 def getAnswer(question: str):
     try:
-        url = "https://openai.api2d.net/v1/chat/completions"
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {zlConfig.fkey}' # <-- 把 fkxxxxx 替换成你自己的 Forward Key，注意前面的 Bearer 要保留，并且和 Key 中间有一个空格。
-        }
-        data = {
-            "model": "gpt-3.5-turbo",
-            "messages": [{"role": "user", "content": question}]
-        }
-
-        response = requests.post(url, headers=headers, json=data)
+        url = f"http://localhost:3030/question?question={question}"
+        headers = { 'Content-Type': 'application/json' }
+        response = requests.get(url, headers=headers)
         data = response.json()
-        return data['choices'][0]['message']['content']
-    except:
+        return data['data']
+    except Exception as e:
         return f'[错误]: 获取答复失败！'
