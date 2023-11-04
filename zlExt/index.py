e18734d5091089ca143db25d94e59bd80a060a91
ByteDance <zhangyunnian@bytedance.com>
2023-11-04 17:27:13 +0800

    modify

--- zlExt/index.py
+++ zlExt/index.py
@@ -2,10 +2,10 @@ import json
 from bridge.reply import Reply, ReplyType
 from bridge.context import ContextType
 from common.log import logger
+from zlExt.models.index import appendMessage, getMessages
 from zlExt.service import getAnswer
 
 taskMap = {}
-messageMap = {}
 MESSAGE_LIMIT = 6
 
 def getZlReply(context):
@@ -44,6 +44,8 @@ def handleGroup(context):
 
     groupId = msg.from_user_nickname or msg.from_user_id
 
+    appendMessage(groupId, f'用户「{msg.actual_user_nickname}」说: {msg.content}')
+
     # 如果群里 @ ai 助手或者群里聊天记录数量达到阈值，就进行回复
     if (msg.is_at):
         if (taskMap.get(groupId)):
@@ -51,33 +53,28 @@ def handleGroup(context):
         taskMap[groupId] = context.content # 记录上次的问题
 
         answer = getAnswer(context.content, groupId, isGroup=True)
+        appendMessage(groupId, f'用户「{msg.to_user_nickname}」说：{answer}')
         del taskMap[groupId]
 
         return Reply(ReplyType.TEXT, answer)
 
-    messageMap.setdefault(groupId, [])
-    messageMap[groupId].append(f'用户「{msg.actual_user_nickname}」说: {msg.content}')
-
-    if (len(messageMap[groupId]) > MESSAGE_LIMIT):
+    messages = getMessages(groupId)
+    if (len(messages) > MESSAGE_LIMIT):
         if (taskMap.get(groupId)): return
         taskMap[groupId] = '系统任务'
 
-        content = json.dumps(messageMap[groupId], ensure_ascii=False)
+        content = json.dumps(messages, ensure_ascii=False)
         content = f"""
-            注意，我在一个聊天群里，我会给你群里最近的一些聊天记录，
-            你需要根据聊天记录，帮我给出一个合理的高质量的回复，我回复后，要么是能活跃群里的气氛，
-            要么能够解释聊天中的一些疑问点，要么是能够给与一个中肯的结论。
-            总之，给出回复后，参与聊天的人，要么能够获得一些快乐，要么能够获得一些知识，要么能够提高一些认知，
-            就是能够有所收获。
+            注意，我是在一个聊天群里，我会给你群里最近的一些聊天记录，
+            你需要根据聊天记录，帮我给出一个的高质量的回复，回复后，总是能够帮助到一些人，
+            比如解答了别人关心的问题；或是安慰了别人，或是给与别人中肯的建议等。
             当然，如果感觉不需要参与回复，你就回复「无需回答」四个字。
-            好了，下面是具体的聊天记录，我之前可能也给了你一些之前的聊天记录，你可以结合之前的聊天记录一起分析，
-            目前最新的聊天记录如下（json 格式内容）：
+            好了，下面是近期具体的聊天记录（json 格式）：
             {content}
         """
         answer = getAnswer(content, groupId, isGroup=True)
-        messageMap[groupId].append(f'用户「{msg.to_user_nickname}」说：{answer}')
+        appendMessage(groupId, f'用户「{msg.to_user_nickname}」说：{answer}')
 
-        del messageMap[groupId]
         del taskMap[groupId]
 
         return Reply(ReplyType.TEXT, answer)
---
+++ zlExt/models/index.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+def getFile(groupId):
+    return f'./{groupId}_message.json'
+
+def appendMessage(groupId, msg, limit = 20):
+    filename = getFile(groupId)
+    data = getMessages()
+    data.append(msg)
+    if (limit > 0): data = data[-limit:]
+
+    with open(filename, 'w') as fp:
+        json.dump(data, fp, indent=2)
+
+def getMessages(groupId):
+    filename = getFile(groupId)
+    if (not os.path.exists(filename)):
+        fp = open(filename, 'w')
+        fp.close()
+
+    try:
+        with open(filename, '+r') as fp:
+            return json.load(fp) or []
+    except Exception as e:
+        print(e)
+        return []
