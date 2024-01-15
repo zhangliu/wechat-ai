87aee554952b045fe945daaa60e4bad163cee8bc
张云念 <zhangyunnian@bytedance.com>
2024-01-15 15:43:06 +0800

    modify

--- zlExt/bots/gemini/history.py
+++ zlExt/bots/gemini/history.py
@@ -5,26 +5,26 @@ def addPrompt(history: list, prompt):
     history.insert(1, {"role": "model", "parts": [{"text": "好的，我明白了！"}]})
     return history
 
-def appendMessage(id, msg, isGropu=False, limit=20):
+def appendMessage(id, msg, isGropu=False, limit=2000):
     item = {
         "role": msg["role"] or "user",
         "parts": [{"text": msg["content"]}]
     }
     historyMsg.appendMessage(id, item, isGropu, limit)
 
-def appendUserMessage(id, content, limit=20):
+def appendUserMessage(id, content, limit=2000):
     msg = {"role": "user", "content": content}
     return appendMessage(id, msg, False, limit)
 
-def appendModelMessage(id, content, limit=20):
+def appendModelMessage(id, content, limit=2000):
     msg = {"role": "model", "content": content}
     return appendMessage(id, msg, False, limit)
 
-def appendGroupUserMessage(id, content, limit=20):
+def appendGroupUserMessage(id, content, limit=2000):
     msg = {"role": "user", "content": content}
     return appendMessage(id, msg, True, limit)
 
-def appendGroupModelMessage(id, content, limit=20):
+def appendGroupModelMessage(id, content, limit=2000):
     msg = {"role": "model", "content": content}
     return appendMessage(id, msg, True, limit)
 
